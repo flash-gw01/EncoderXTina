9759c012387d158a14368d91d6a54230cc2ec9d3
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-09-08 14:57:12 +0530

    Update utils.py

--- bot/helper_funcs/utils.py
+++ bot/helper_funcs/utils.py
@@ -9,6 +9,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
 import os, asyncio, pyrogram, psutil, platform, time
+from speedtest import Speedtest
 from bot import data
 from bot.plugins.incoming_message_fn import incoming_compress_message_f
 from pyrogram.types import Message
@@ -61,4 +62,38 @@ async def sysinfo(e):
                                  f"<b>🖥 CPU:</b> {cpuUsage}%\n"
                                  f"<b>🎮 RAM:</b> {int(ram_stats.percent)}%\n"
                                  f"<b>💿 DISK:</b> {int(disk.percent)}%")
+
+def speed_convert(size, byte=True):
+    """Hi human, you can't read bytes?"""
+    if not byte: size = size / 8 # byte or bit ?
+    power = 2 ** 10
+    zero = 0
+    units = {0: "B/s", 1: "KB/s", 2: "MB/s", 3: "GB/s", 4: "TB/s"}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+def speedtest(update, context):
+    speed = sendMessage("Running Speed Test. Wait about 20 secs.", context.bot, update.message)
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = (result['share'])
+    string_speed = f'''
+<b>Server</b>
+<b>Name:</b> <code>{result['server']['name']}</code>
+<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>ISP:</b> <code>{result['client']['isp']}</code>
+
+<b>SpeedTest Results</b>
+<b>Upload:</b> <code>{speed_convert(result['upload'], False)}</code>
+<b>Download:</b>  <code>{speed_convert(result['download'], False)}</code>
+<b>Ping:</b> <code>{result['ping']} ms</code>
+<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+'''
     
