d32ec51ebe2d9976141e0c3b92638204c9565d4b
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-09-09 19:48:15 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -12,11 +12,11 @@ class Localisation:
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     
     
-    DOWNLOAD_START = "Downloading ....📥 \n"
+    DOWNLOAD_START = "Downloading....📥 \n"
     
-    UPLOAD_START = "Uploading ....📤 \n"
+    UPLOAD_START = "Uploading....📤 \n"
     
-    COMPRESS_START = "Trying to encode ....📀"
+    COMPRESS_START = "Trying to encode....📀"
     
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     
